1a4449a0570ecfa538c68a2b48df30cb6ef0add6
DylanDorey <dedorey@csuchico.edu>
2024-04-11 12:36:40 -0700

    Started Melee

--- Gauntlet/Assets/Scripts/Player Scripts/Warrior/WarriorMelee.cs
+++ Gauntlet/Assets/Scripts/Player Scripts/Warrior/WarriorMelee.cs
@@ -4,16 +4,27 @@ using UnityEngine;
 
 public class WarriorMelee : Warrior, IPlayerBehavior
 {
+    private RaycastHit hit;
+    private Vector3 rayDirection = new Vector3.forward();
+    private float rayDistance = 2f;
+
     public void PlayerBehavior(PlayerController player)
     {
-        Melee();
+        StartCoroutine(Melee());
     }
 
     /// <summary>
     /// allows the player to hit directly in front of them
     /// </summary>
-    private void Melee()
-    { 
-        
+    private IEnumerator Melee()
+    {
+        while(true)
+        {
+            if (Physics.Raycast(transform.position, rayDirection, out hit, rayDistance))
+            {
+                Debug.DrawRay(transform.position, rayDirection, Color.green);
+            }
+            yield return new WaitForSeconds(1.0f);
+        }
     }
 }
