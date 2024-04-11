266a74502c1e6761c7c607aa739a315de40e5abe
idiazjr <idiazjr@csuchico.edu>
2024-04-11 13:33:23 -0700

    Adding Melee and Shooting scripts

---
+++ Gauntlet/Assets/Scripts/Player Scripts/Valkyrie/Valkyrie.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Valkyrie : MonoBehaviour, IValkyrieMelee, IValkyrieShoot
+{
+    public float bulletSpeed = 5f;
+    public Rigidbody projectile;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
