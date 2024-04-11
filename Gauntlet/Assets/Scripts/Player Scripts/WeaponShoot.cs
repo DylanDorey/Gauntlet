b28e946404acf31d787d6cccc7a2cca00edd25fc
idiazjr <idiazjr@csuchico.edu>
2024-04-11 13:21:04 -0700

    Update WeaponShoot.cs

--- Gauntlet/Assets/Scripts/Player Scripts/WeaponShoot.cs
+++ Gauntlet/Assets/Scripts/Player Scripts/WeaponShoot.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class WeaponShoot : MonoBehaviour
 {
+    public float bulletSpeed = 5f;
+    public Rigidbody projectile;
+    public GameObject spawnPoint;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,9 @@ public class WeaponShoot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetButtonDown("Right Trigger"))
+        { 
+            
+        }
     }
 }
