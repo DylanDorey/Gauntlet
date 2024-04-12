1fa3221e5ced96802e815cfc4daeb8daf54ebbfe
DylanDorey <dedorey@csuchico.edu>
2024-04-11 18:52:24 -0700

    Axe is not throwable using right trigger

---
+++ Gauntlet/Assets/Scripts/Player Scripts/Warrior/Axe.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Author: [Dorey, Dylan]
+ * Last Updated: [4/11/2024]
+ * [A throwable axe that the warrior can throw]
+ */
+
+public class Axe : MonoBehaviour
+{
+    [Range(1f, 10f)]
+    [SerializeField]
+    private float axeSpeed;
+
+    [Range(1f, 10f)]
+    public float axeDamage;
+
+    private float rotateSpeed = 10f;
+
+    public Vector3 moveDirection;
+
+    private void FixedUpdate()
+    {
+        Move();
+        RotateAxe();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            //collision.gameObject.GetComponent<Enemy>().TakeDamage(axeDamage);
+        }
+    }
+
+    private void Move()
+    {
+        transform.position += (moveDirection * (axeSpeed * Time.deltaTime));
+    }
+
+    private void RotateAxe()
+    {
+        transform.Rotate(0f, rotateSpeed, 0f);
+    }
+}
--- Gauntlet/Assets/Scripts/Player Scripts/Warrior/Warrior.cs
+++ Gauntlet/Assets/Scripts/Player Scripts/Warrior/Warrior.cs
@@ -2,10 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Author: [Dorey, Dylan]
+ * Last Updated: [4/11/2024]
+ * [Warrior class for the character Thor]
+ */
+
 public class Warrior : MonoBehaviour
 {
     [Range(1f, 10f)]
     public float meleeDelay;
+
+    [Range(1f, 10f)]
+    public float throwDelay;
 
     [Range(1f, 10f)]
     public int meleeDamage;
@@ -16,9 +25,12 @@ public class Warrior : MonoBehaviour
     public RaycastHit hit;
     public Vector3 rayDirection;
 
+    public GameObject axePrefab;
+
     public void Start()
     {
         GetComponent<PlayerController>().meleeBehavior = GetComponent<WarriorMelee>();
+        GetComponent<PlayerController>().shootBehavior = GetComponent<ThrowAxe>();
     }
 
     private void Update()
