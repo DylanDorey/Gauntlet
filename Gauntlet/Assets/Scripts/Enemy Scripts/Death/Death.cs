a6258518a261db17b13b0965d1b628fdc3c55825
idiazjr <idiazjr@csuchico.edu>
2024-04-25 12:59:43 -0700

    Saving Scene

--- Gauntlet/Assets/Scripts/Enemy Scripts/Death/Death.cs
+++ Gauntlet/Assets/Scripts/Enemy Scripts/Death/Death.cs
@@ -2,7 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Death : MonoBehaviour
+public class Death : Enemy
 {
-    
+    private void Awake()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
 }
