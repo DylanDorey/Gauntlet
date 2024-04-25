38c70a916d6e0bd1b55c67978a4c0912bf1fe337
idiazjr <idiazjr@csuchico.edu>
2024-04-25 12:13:41 -0700

    Revert "Testing dummy follow code"
    This reverts commit 98a849b5a882e9caa36642de3938c530e42dae7e.

--- Gauntlet/Assets/Scripts/Enemy Scripts/Ghost/Ghost.cs
+++ Gauntlet/Assets/Scripts/Enemy Scripts/Ghost/Ghost.cs
@@ -2,34 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using Newtonsoft.Json.Bson;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Ghost : MonoBehaviour
 {
+    [SerializeField] Transform positionTransform;
+    private NavMeshAgent navMeshAgent;
     private bool _isChasingPlayer = false;
     private Transform player;
 
-    private void Update()
+    private void Start()
     {
-        if (_isChasingPlayer && player != null)
-        { 
-            transform.LookAt(player);
-            transform.Translate(Vector3.forward * Time.deltaTime * 1f);
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            player = other.transform;
-            _isChasingPlayer = true;
-        }
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.CompareTag("Player"))
-        {
-            _isChasingPlayer = false;
-        }
+        player = GameObject.FindGameObjectsWithTag("Player").transform;
     }
 }
