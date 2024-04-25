4d575da7bcd62224ce18de8ef9651fa056c3cc27
idiazjr <idiazjr@csuchico.edu>
2024-04-25 12:38:23 -0700

    Update Death.cs

--- Gauntlet/Assets/Scripts/Enemy Scripts/Death/Death.cs
+++ Gauntlet/Assets/Scripts/Enemy Scripts/Death/Death.cs
@@ -4,19 +4,5 @@ using UnityEngine;
 
 public class Death : MonoBehaviour
 {
-<<<<<<< Updated upstream
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-=======
     
->>>>>>> Stashed changes
 }
