6df9c2df84d3d4412331d7eb2eb00b9b7ab0658c
idiazjr <idiazjr@csuchico.edu>
2024-05-09 11:37:18 -0700

    Bugged but working lobber script

--- Gauntlet/Assets/Scripts/Enemy Scripts/Lobber/Lobber.cs
+++ Gauntlet/Assets/Scripts/Enemy Scripts/Lobber/Lobber.cs
@@ -2,17 +2,78 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*
+ * Author: [Diaz, Ismael]
+ * Last Updated: 5/9/24
+ * [Functions for Lobber]
+ */
+
 public class Lobber : Enemy
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject projectilePrefab;
+    public Transform throwPoint;
+    public float throwForce = 10f;
+    public float throwDelay = 2f;
+
+    private bool canThrow = true;
+
+    private void FixedUpdate()
     {
-        
+        if (isAggro)
+        {
+            FindNearestPlayer();
+            Move();
+            if (canThrow)
+            {
+                StartCoroutine(ThrowProjectileWithDelay());
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator ThrowProjectileWithDelay()
     {
-        
+        // Set flag to prevent multiple throws before delay
+        canThrow = false;
+        yield return new WaitForSeconds(throwDelay);
+
+        // Calculate the direction towards the player
+        Vector3 directionToPlayer = (targetPos - throwPoint.position).normalized;
+
+        // Create and throw the projectile
+        GameObject projectile = Instantiate(projectilePrefab, throwPoint.position, Quaternion.identity);
+        Rigidbody rb = projectile.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            // Apply force towards the player
+            rb.AddForce(directionToPlayer * throwForce, ForceMode.Impulse);
+        }
+
+        canThrow = true; // Reset flag to allow next throw
+    }
+
+    void FindNearestPlayer()
+    {
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        float minDistance = Mathf.Infinity;
+        GameObject nearestPlayer = null;
+
+        foreach (GameObject player in players)
+        {
+            float distance = Vector3.Distance(transform.position, player.transform.position);
+            if (distance < minDistance)
+            {
+                minDistance = distance;
+                nearestPlayer = player;
+            }
+        }
+
+        if (nearestPlayer != null && minDistance <= enemyRadius)
+        {
+            targetPos = nearestPlayer.transform.position;
+        }
+        else
+        {
+            targetPos = Vector3.zero;
+        }
     }
 }
